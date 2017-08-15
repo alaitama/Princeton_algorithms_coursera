c7fd08bbcaeeb9a4a3b999ba8425bee5232dd5dd
ramsys <alblaita@gmail.com>
2017-08-15 21:42:21 +0200

    Percolation assignment and pom

---
+++ src/main/java/Percolation.java
@@ -0,0 +1,111 @@
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+/**
+ * Created by ramborg on 11/08/17.
+ */
+public class Percolation {
+
+    private final WeightedQuickUnionUF weightedQuickUnionUF;
+    private final WeightedQuickUnionUF weightedQuickUnionUFisFull;
+    private final int gridSize;
+    private final int pointsSize;
+    private int numOpens;
+
+    private boolean[][] openedSites;
+
+    // create n-by-n grid, with all sites blocked
+    public Percolation(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        pointsSize = n*n+2;
+        weightedQuickUnionUF = new WeightedQuickUnionUF(pointsSize);
+        weightedQuickUnionUFisFull = new WeightedQuickUnionUF(pointsSize-1);
+        gridSize = n;
+
+        openedSites = new boolean[n][n];
+
+        // Connect virtual top bottom sites
+        for (int i = 1; i <= gridSize; i++) {
+            weightedQuickUnionUF.union(0, i);
+            weightedQuickUnionUF.union(pointsSize-1, pointsSize-i-1);
+            weightedQuickUnionUFisFull.union(0, i);
+        }
+    }
+
+    // open site (row, col) if it is not open already
+    public void open(int row, int col) {
+        checkParams(row, col);
+
+        if (!isOpen(row, col)) {
+            int pointPosition = getPointPosition(row, col);
+            // North
+            if (row > 1 && isOpen(row -1, col)) {
+                weightedQuickUnionUF.union(pointPosition, pointPosition - gridSize);
+                weightedQuickUnionUFisFull.union(pointPosition, pointPosition - gridSize);
+            }
+            // South
+            if (row < gridSize && isOpen(row + 1, col)) {
+                weightedQuickUnionUF.union(pointPosition, pointPosition + gridSize);
+                weightedQuickUnionUFisFull.union(pointPosition, pointPosition + gridSize);
+
+            }
+            // East
+            if (col > 1 && isOpen(row, col - 1)) {
+                weightedQuickUnionUF.union(pointPosition, pointPosition - 1);
+                weightedQuickUnionUFisFull.union(pointPosition, pointPosition - 1);
+
+            }
+            // West
+            if (col < gridSize && isOpen(row, col + 1)) {
+                weightedQuickUnionUF.union(pointPosition, pointPosition + 1);
+                weightedQuickUnionUFisFull.union(pointPosition, pointPosition + 1);
+
+            }
+
+            numOpens++;
+            openedSites[row-1][col-1] = true;
+        }
+    }
+
+    // is site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        checkParams(row, col);
+
+        return openedSites[row-1][col-1];
+    }
+
+    // is site (row, col) full?
+    public boolean isFull(int row, int col)  {
+        checkParams(row, col);
+
+        return isOpen(row, col) && weightedQuickUnionUFisFull.connected(0, getPointPosition(row, col));
+    }
+
+    // number of open sites
+    public int numberOfOpenSites() {
+        return numOpens;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return numOpens > 0 && weightedQuickUnionUF.connected(0, pointsSize-1);
+    }
+
+    private void checkParams(int row, int col) {
+        if (row < 1 || row > gridSize || col < 1 || col > gridSize) {
+            throw new IllegalArgumentException("Not accesed for row " + row + " and col " + col);
+        }
+    }
+
+    private int getPointPosition(int row, int col) {
+        return ((row-1)*gridSize)+col;
+    }
+
+    public static void main(String[] args)  {
+        StdOut.println("Nothing implemented");
+    }
+}
---
+++ src/main/java/PercolationStats.java
@@ -0,0 +1,126 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+/**
+ * Created by ramborg on 11/08/17.
+ */
+public class PercolationStats {
+
+    private final int n;
+    private final int trials;
+    private final int[] numOpenSites;
+    private double mean;
+    private double stdDev;
+
+    // perform trials independent experiments on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        numOpenSites = new int[trials];
+        this.trials = trials;
+        this.n = n;
+
+        for (int i = 0; i < trials; i++) {
+            Percolation percolation = new Percolation(n);
+
+            while (!percolation.percolates()) {
+                int row = StdRandom.uniform(1, n+1);
+                int col = StdRandom.uniform(1, n+1);
+
+                percolation.open(row, col);
+            }
+
+            numOpenSites[i] = percolation.numberOfOpenSites();
+        }
+
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+
+        if (trials == 1) {
+            return Double.NaN;
+        }
+        else {
+            mean = StdStats.mean(numOpenSites) / ((double) (n*n));
+            return mean;
+        }
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+
+        if (trials == 1) {
+            return Double.NaN;
+        }
+        else {
+            stdDev = StdStats.stddev(numOpenSites) / ((double) (n*n));
+            return stdDev;
+        }
+
+    }
+
+    // low  endpoint of 95% confidence interval
+    public double confidenceLo() {
+
+        if (trials == 1) {
+            return Double.NaN;
+        }
+        else {
+
+            return getMean() - ((1.96 * getStdDev()) / Math.sqrt(trials));
+
+        }
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+
+        if (trials == 1) {
+            return Double.NaN;
+        }
+        else {
+            return getMean() + ((1.96 * getStdDev()) / Math.sqrt(trials));
+        }
+
+    }
+
+    private double getMean() {
+        if (mean != 0) {
+            return mean;
+        }
+        else {
+            return  mean();
+        }
+    }
+
+    private double getStdDev() {
+        if (stdDev != 0) {
+            return stdDev;
+        }
+        else {
+            return  stddev();
+        }
+    }
+
+    // test client (described below)
+    public static void main(String[] args)  {
+
+        PercolationStats percolationStats = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+
+        StdOut.print("mean                    = ");
+        StdOut.println(percolationStats.mean());
+        StdOut.print("stddev                  = ");
+        StdOut.println(percolationStats.stddev());
+        StdOut.print("95% confidence interval = [");
+        StdOut.print(percolationStats.confidenceLo());
+        StdOut.print(", ");
+        StdOut.print(percolationStats.confidenceHi());
+        StdOut.println("]");
+
+
+    }
+}
